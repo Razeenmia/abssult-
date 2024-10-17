47fe4c36377fe90823bee3d7308769aa45a95c15
Razeenmia <rashaad.mia@gmial.com>
2024-10-17 19:50:29 +0200

    cool

---
+++ mai.py
@@ -0,0 +1,6 @@
+def absolute(number):
+    if  number >0:
+        return number
+    else:
+        return -number
+print(absolute(-566666666666666666666666666666666666666666666666666666666666666666666666))
